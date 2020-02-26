f6ecf5efc7545dc6a145b98ff4adc075dc936e03
Arslan <rslnkrmt2552@gmail.com>
2020-02-26 12:36:30 +0300

    C# solutions

---
+++ csharp/countingValleys/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace countingValleys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "DDUUDDUDUUUD";
+            var len = 12;
+            var res = countingValleys(len, s);
+            Console.WriteLine($"No of valleys: {res} {Environment.NewLine}");
+        }
+
+        static int countingValleys(int n, string s)
+        {
+            var i = 0;
+            var valleys = 0;
+            var insideValley = false;
+            var level = 0;
+            while (i < (n - 1))
+            {
+
+                if (s[i] == 'U')
+                    level++;
+                else level--;
+
+                if ((!insideValley) && (level < 0))
+                {
+                    valleys++;
+                    insideValley = true;
+                }
+                else if ((insideValley) && (level >= 0))
+                {
+                    insideValley = false;
+                }
+
+                i++;
+            }
+
+            return valleys;
+        }
+    }
+}
---
+++ csharp/sock_merchant/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sock_merchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+            int res = sockMerchant(n, ar);
+            Console.WriteLine(res);
+        }
+
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            int count = 0;
+            var table = new Dictionary<int, int>();
+            foreach (var el in ar)
+            {
+                if (table.ContainsKey(el))
+                {
+                    table[el] = table[el] + 1;
+                }
+                else
+                {
+                    table.Add(el, 1);
+                }
+            }
+
+            foreach (var key in table.Keys)
+            {
+                count += table[key] / 2;
+            }
+            
+            
+            return count;
+        }
+    }
+}
