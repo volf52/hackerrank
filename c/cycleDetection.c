6f6bc0b96d5deb017c236d3104bd8687cc61f64b
Arslan <rslnkrmt2552@gmail.com>
2020-03-05 10:22:21 +0300

    Print cycle list

---
+++ c/cycleDetection.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct SinglyLinkedListNode SinglyLinkedListNode;
+
+struct SinglyLinkedListNode {
+    int data;
+    SinglyLinkedListNode* next;
+};
+
+SinglyLinkedListNode* createNode(int);
+void printCycleList(SinglyLinkedListNode* node, char* sep, FILE* fptr, int n1, int n2);
+SinglyLinkedListNode* createCycleList(int* arrOne, int n1, int* arr2, int n2);
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2);
+
+void print(SinglyLinkedListNode* node, int n1, int n2)
+{
+    printCycleList(node, "->", stdout, n1, n2);
+    printf("\n\n");
+}
+
+int main()
+{
+    SinglyLinkedListNode* head;
+    int arr1[] = { 1, 2, 3 };
+    int arr2[] = { 4, 5, 6 };
+
+    head = createCycleList(arr1, 3, arr2, 3);
+    print(head, 3, 3);
+
+    return 0;
+}
+
+SinglyLinkedListNode* createCycleList(int* arrOne, int n1, int* arr2, int n2)
+{
+    int i;
+    SinglyLinkedListNode *head, *curr, *tmp;
+
+    if (!(n1 && n2))
+        return NULL;
+
+    head = createNode(arrOne[0]);
+    curr = head;
+    for (i = 1; i < n1; i++) {
+        curr->next = createNode(arrOne[i]);
+        curr = curr->next;
+    }
+
+    tmp = curr;
+
+    for (i = 0; i < n2; i++) {
+        curr->next = createNode(arr2[i]);
+        curr = curr->next;
+    }
+
+    curr->next = tmp;
+
+    return head;
+}
+
+SinglyLinkedListNode* createNode(int nodeData)
+{
+    SinglyLinkedListNode* node = (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode));
+
+    node->data = nodeData;
+    node->next = NULL;
+
+    return node;
+}
+
+void printCycleList(SinglyLinkedListNode* node, char* sep, FILE* fptr, int n1, int n2)
+{
+    int i;
+    char *s1, *s2;
+
+    if (!node)
+        return;
+
+    i = 0;
+    s1 = "{Cycle start}";
+    s2 = "{Cycle end}";
+
+    while (i < n1) {
+        fprintf(fptr, "%d", node->data);
+
+        node = node->next;
+
+        if (node) {
+            fprintf(fptr, "%s", sep);
+        }
+        i++;
+    }
+    fprintf(fptr, "%s", s1);
+
+    i = 0;
+    while (i < n2) {
+        fprintf(fptr, "%d", node->data);
+
+        node = node->next;
+
+        if (node) {
+            fprintf(fptr, "%s", sep);
+        }
+        i++;
+    }
+    fprintf(fptr, "%s", s2);
+}
